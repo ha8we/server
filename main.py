1ddba76b5082dcdd7b25cd9ba568808e07087bde
ha8we <82208165+ha8we@users.noreply.github.com>
2022-04-08 16:20:22 +0200

    i csere

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def on_message(mqtt_client, user_data, message):
 """
 def getdevice(x):
     cursor = mydb.cursor()
-    cursor.execute("SELECT * FROM Device")
+    cursor.execute("SELECT `MAC`,`VL1`,`VL2`,`VL3` FROM `Device` WHERE 1")
     result = cursor.fetchall()
     return result
 
@@ -85,14 +85,36 @@ def savesqlpwrmeter(x):
         except:
             print("HIBA " + query)
 
-def savesqlclientpwr(x,y):
+def savesqlclientpwr(x,mac):
     cursor = mydb.cursor()
 
     if (len(x) == 17):
         try:
-            query = "INSERT INTO `"+y+"` (`LastData`, `MAC`, `U12`, `U23`, `U31`, `I1`, `I2`, `I3`, `INe`, `Freq`, `PF`, `P`, `Q`, `S`, `Ph`, `Qh`, `Sh`) VALUES (TIMESTAMP(CURRENT_TIMESTAMP),'" + \
-                    x[0] + "','" + x[1] + "', '" + x[2] + "', '" + x[3] + "', '" + x[4] + "', '" + x[5] + "', '" + x[
-                        6] + "', '" + x[7] + "', '" + x[8] + "', '" + x[9] + "', '" + x[10] + "', '" + x[11] + "', '" + \
+            I1 = 0
+            I2 = 0
+            I3 = 0
+            if (mac[1] == 1):
+                I1 = x[4]
+            elif (mac[1] == 2):
+                I2 = x[4]
+            elif (mac[1] == 3):
+                I3 = x[4]
+
+            if (mac[2] == 1):
+                I1 = x[5]
+            elif (mac[2] == 2):
+                I2 = x[5]
+            elif (mac[2] == 3):
+                I3 = x[5]
+
+            if (mac[3] == 1):
+                I1 = x[6]
+            elif (mac[3] == 2):
+                I2 = x[6]
+            elif (mac[3] == 3):
+                I3 = x[6]
+            query = "INSERT INTO `"+mac[0]+"` (`LastData`, `MAC`, `U12`, `U23`, `U31`, `I1`, `I2`, `I3`, `INe`, `Freq`, `PF`, `P`, `Q`, `S`, `Ph`, `Qh`, `Sh`) VALUES (TIMESTAMP(CURRENT_TIMESTAMP),'" + \
+                    x[0] + "','" + x[1] + "', '" + x[2] + "', '" + x[3] + "', '" + str(I1) + "', '" + str(I2) + "', '" + str(I3)+ "', '" + x[7] + "', '" + x[8] + "', '" + x[9] + "', '" + x[10] + "', '" + x[11] + "', '" + \
                     x[12] + "', '" + x[13] + "', '" + x[14] + "', '" + x[15] + "')"
             cursor.execute(query)
             mydb.commit()
@@ -100,15 +122,38 @@ def savesqlclientpwr(x,y):
         except:
             print("nem megfelelo uzenet"+query)
 
-def updateclient(x):
+def updateclient(x,mac):
     cursor = mydb.cursor()
+    I1=0
+    I2 = 0
+    I3 = 0
+    if (mac[1]==1):
+        I1=x[4]
+    elif (mac[1]==2):
+        I2 = x[4]
+    elif (mac[1] == 3):
+        I3 = x[4]
+
+    if (mac[2]==1):
+        I1=x[5]
+    elif (mac[2]==2):
+        I2 = x[5]
+    elif (mac[2] == 3):
+        I3 = x[5]
+
+    if (mac[3]==1):
+        I1=x[6]
+    elif (mac[3]==2):
+        I2 = x[6]
+    elif (mac[3] == 3):
+        I3 = x[6]
 
     if (len(x) == 17):
         try:
-            query ="UPDATE `Device` SET `I1` = "+ x[4] +" ,`I2` = "+ x[5]+" ,`I3` = "+ x[6] +" WHERE  `MAC`=" + x[0]
+            query ="UPDATE `Device` SET `I1` = '" + str(I1) +"' ,`I2` = '" + str(I2)+"' ,`I3` = '" + str(I3) +"' WHERE  `MAC`='" + str(x[0])+"'"
             cursor.execute(query)
             mydb.commit()
-            print(query)
+
         except:
             print("nem megfelelo uzenet" + query)
 
@@ -121,17 +166,53 @@ def Readyclient(x,y):
         print(x[0]+" Töltésre kész! állapota")
     except:
         print("HIBA " +query)
+def getcharge():
+    cursor = mydb.cursor()
+    cursor.execute("SELECT `MAC`,`I1`,`I2`,`I3`,`Priority` FROM `Device` WHERE `Charging`=1")
+    result = cursor.fetchall()
+    return result
 
 def PWRcontrol():
     cursor = mydb.cursor()
     try:
-        query = "UPDATE `Device` SET `Ready` = " + str(y) + " WHERE  `MAC`=" + x[0]
+        query = "SELECT `Max I1`,`Max I2`,`Max I3`,`I1`,`I2`,`I3`,`LastData`, `Tartalek`  FROM `System` WHERE 1"
         cursor.execute(query)
+        result = cursor.fetchall()
         mydb.commit()
-        print(x[0] + " Töltésre kész! állapota")
+        tmp=result[0]
+        I1max=tmp[0]
+        I2max = tmp[1]
+        I3max = tmp[2]
+        I1 = tmp[3]
+        I2 = tmp[4]
+        I3 = tmp[5]
+        tart=tmp[7]
+        print("A maximális áramok I1:" +str(tmp[0])+ " I2: " + str(tmp[1]) +" I3: " + str(tmp[2]))
+        print("Aktuális áramok I1:" + str(I1) + " I2: " + str(I2) + " I3: " + str(I3))
+        TI1=0
+        TI2=0
+        TI3=0
+        dI1=0
+        dI2=0
+        dI3=0
+        szummprio=0
+        if I1>I1max or I2>I2max or I3>I3max:    #be kell avatkozni
+            aktolt=getcharge()
+            db= len(aktolt)
+            for y in range(0,db):
+                tmp=aktolt[db-1]
+                TI1=TI1+int(tmp[1])
+                TI2 = TI2 + int(tmp[2])
+                TI3 = TI3 + int(tmp[3])
+                szummprio= szummprio+int(tmp[4])
+                print(int(TI1))
+            dI1=  I1max-I1-TI1
+            dI2 = I2max-I2 - TI2
+            dI3 = I3max-I3 - TI3          #szabad áramok
+            print(TI1, TI2 , TI3, dI1, dI2, dI3,szummprio)
     except:
         print("HIBA " + query)
-    print(x)
+
 
 
 def subscribe(client: mqtt_client):
@@ -140,8 +221,8 @@ def subscribe(client: mqtt_client):
         payload = msg.payload.decode()
         x = payload.split(";")
         if x[0] == MacPowermeter:
-          #  PWRcontrol(x)
             savesqlpwrmeter(x)
+            PWRcontrol()
         else:
 
             if x[1] == "START" : #itt mindenkit meg kell kérni egy státuszra/vagy leszajuk és az utolsó adatok szerint járunk el
@@ -158,9 +239,10 @@ def subscribe(client: mqtt_client):
                 print(len(device))
                 for y in range(0, len(device)):    #UPDATE `Device` SET `I1max` = 15 WHERE  `MAC`=112233445566
                     mac=device[y]
+
                     if mac[0]==x[0]:
-                        savesqlclientpwr(x,mac[0])
-                        updateclient(x)
+                        savesqlclientpwr(x,mac)
+                        updateclient(x,mac)
                         print(mac)
                         print(x[0])
 
